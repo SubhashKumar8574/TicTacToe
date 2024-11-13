a2d93cf3e1b16d35c80d6a606ab75b622fb626e5
SubhashKumar8574 <Subhash.kumar622002@gmail.com>
2024-11-13 12:17:12 +0530

    Initialised Project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import TicTacToe from './components/tictactoe/TicTacToe'
+
+const App = () => {
+  return (
+    <div>
+      <TicTacToe />
+      
+    </div>
+
+  )
+}
+
+export default App
